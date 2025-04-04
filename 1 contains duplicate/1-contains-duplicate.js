db77b0952d807806ea8561c9934a21f2849002c0
Pranay Patel <pranaypatel008@gmail.com>
2025-04-05 00:28:59 +0530

    use Set in contain duplicate js

--- 1 contains duplicate/1-contains-duplicate.js
+++ 1 contains duplicate/1-contains-duplicate.js
@@ -2,16 +2,20 @@
 
 const readline = require("readline");
 
+// const containsDuplicate = function (nums) {
+//   let map = new Map();
+//   for (let i = 0; i < nums.length; i++) {
+//     if (map.has(nums[i])) {
+//       return true;
+//     } else {
+//       map.set(nums[i], 1);
+//     }
+//   }
+//   return false;
+// };
+
 const containsDuplicate = function (nums) {
-  let map = new Map();
-  for (let i = 0; i < nums.length; i++) {
-    if (map.has(nums[i])) {
-      return true;
-    } else {
-      map.set(nums[i], 1);
-    }
-  }
-  return false;
+  return nums.length !== new Set(nums).size;
 };
 
 // take input from user in console
